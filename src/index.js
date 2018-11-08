85af871935bf26c1e5e874ec4632b54030b10e5f
Karla Buckner <kmb2662@gmail.com>
2018-11-08 09:51:32 -0800

    now incorporate jsx and transpiled code

--- src/index.js
+++
@@ -1,9 +0,0 @@
-var title = React.createElement('h1', {}, 'Field Tryp');
-var about = React.createElement('h3', {}, 'Making day trips possible and environmentally more friendly. See the places you can go in cities across the US.');
-var city = React.createElement('h2', {}, 'Select a location');
-var app = React.createElement('div', {}, title, about, city);
-
-ReactDOM.render(
-  app,
-  document.getElementById('react-app-root')
-);
---
+++ src/index.jsx
@@ -0,0 +1,60 @@
+'use strict';
+
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Field Tryp'
+  ),
+  React.createElement(
+    'h3',
+    null,
+    'Making day trips possible and environmentally more friendly. See the places you can go in cities across the US.'
+  ),
+  React.createElement(
+    'h2',
+    null,
+    'Select a Location'
+  ),
+  React.createElement(
+    'ul',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Portland'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Seattle'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Denver'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Asheville'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Austin'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'New York City'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Chicago'
+    )
+  )
+), document.getElementById('react-app-root'));
